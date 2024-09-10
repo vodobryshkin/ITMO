ed2b179808da1673a0709794636314b56c2ef028
Vladimir Dobryshkin <110555032+vodobryshkin@users.noreply.github.com>
2024-09-10 20:20:29 +0300

    Добавление кода  первой лабораторной

---
+++ 1 курс/1 семестр/Программирование/Лабораторная №1/Lab1.java
@@ -0,0 +1,57 @@
+public class Lab1 {
+   // статичный метод для вычисления элемента матрицы
+   public static float calculateElement(short r, float x) {
+      switch (r) {
+         case 11: 
+            return (float) Math.atan(Math.sin(x));
+         case 4, 6, 7, 12, 13, 15, 16, 17:
+            return 2 * (float) Math.atan(Math.sin(x));
+         default:
+            // заметим, что f(x) = e^ln(x) <=> f(x) = x
+            // тогда нужно учитывать лишь значение арккосинуса
+            return (float) Math.acos(Math.pow((x + 2) / 26, 2f));
+      }
+   }
+   
+   // статичный метод для вывода матрицы
+   public static void printMatrix(float[][] m) {
+      for (var i = 0; i < m.length; i++) {
+         for (var j = 0; j < m[i].length; j++) {
+            System.out.printf("%.03f\t", m[i][j]);
+         }
+         System.out.printf("\n");
+      }
+   }
+
+   public static void main(String[]  args) {
+      // создание одномерного массива r типа short 
+      short[] r = new short[17];
+      
+      // заполнение массива r числами от 4 до 20
+      for (short i = 4; i <= 20; i++) {
+         r[i - 4] = i;
+      }
+
+      // создание одномерного массива x типа float
+      float[] x = new float[15];
+
+      // заполнение массива r случайными числами в диапозоне от -11.0 до 15.0
+      for (var i = 0; i < 15; i++) {
+         x[i] = (float)((Math.random() * (15.0 + 11.0)) + -11.0);
+      }
+      
+      // создание двумерного массива m
+      float[][] m = new float[17][15];
+
+      // вычисление элементов массива m
+      for (var i = 0; i < 17; i++) {
+         for (var j = 0; j < 15; j++) {
+            // вычисление элемента матрицы с помощью статичного метода calculateElement
+            m[i][j] = calculateElement(r[i], x[j]);
+         }
+      }
+
+      // вывод матрицы с помощью статичного метода printMatrix
+      printMatrix(m);
+   }
+}

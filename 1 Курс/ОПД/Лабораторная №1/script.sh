8108b626fbc432c2fb5c5833ba0d0e78f38594b0
Vladimir Dobryshkin <110555032+vodobryshkin@users.noreply.github.com>
2024-10-28 08:11:30 +0300

    Update script.sh

--- 1 Курс/ОПД/Лабораторная №1/script.sh
+++ 1 Курс/ОПД/Лабораторная №1/script.sh
@@ -1,23 +1,23 @@
-#!/bin/bash
-chmod +x script.sh
+#5001
+./cleaner.sh # удаление файлов с предыдущего запуска скрипта
 
 #-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # Первый пункт задания. Создание файлов и каталогов.
 #-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
+mkdir lab0
 cd lab0
 
 # Работа с каталогом foongus4 и файлами/каталогами внутри него
 mkdir foongus4
-   mkdir foongus4/sawsbuck
-   mkdir foongus4/carvanha
-   echo "Тип диеты Carnivore" > foongus4/houndoom # создание и заполнение файла houndoom содержимым согласно условию
+mkdir foongus4/sawsbuck
+mkdir foongus4/carvanha
+echo "Тип диеты Carnivore" > foongus4/houndoom # создание и заполнение файла houndoom содержимым согласно условию
 
 # Работа с каталогом mienfoo6 и файлами/каталогами внутри него 
 mkdir mienfoo6
-   mkdir mienfoo6/galvantula
-   mkdir mienfoo6/hoothoot
-   cat << EOF > mienfoo6/munchlax
+mkdir mienfoo6/galvantula
+mkdir mienfoo6/hoothoot
+cat << EOF > mienfoo6/munchlax
 Развитые способности
 Gluttony
 EOF
@@ -27,14 +27,14 @@ echo "satk=4 sdef=4 spd=9" > poliwag5 # заполнение файла соде
 
 # Работа с каталогом poochyena7 и файлами/каталогами внутри него
 mkdir poochyena7
-   mkdir poochyena7/kabutops
-   mkdir poochyena7/magneton
-   echo "weight-20.9 height=47.0 atk=9 def=12" > poochyena7/weezing
-   cat << EOF > poochyena7/galvantula
+mkdir poochyena7/kabutops
+mkdir poochyena7/magneton
+echo "weight-20.9 height=47.0 atk=9 def=12" > poochyena7/weezing
+cat << EOF > poochyena7/galvantula
 Тип диеты
 :Carnivore
 EOF
-   cat << EOF > poochyena7/blaziken
+cat << EOF > poochyena7/blaziken
 Ходы
 Blast Burn Body Slam Bounce Counter Double-Edge Dual Chop Dynamicpunch
 Fire Pledge Fire Punch Focus Punch Fury Cutter Heat Wave Helping Hand
@@ -83,40 +83,44 @@ chmod u=,g=rw,o=w wailord4
 #-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # Третий пункт задания. Копирование файлов и создание ссылок.
 #-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-cd ..
-cp lab0/poliwag5 lab0/mienfoo6/munchlaxpoliwag
-ln -sf lab0/poliwag5 lab0/mienfoo6/munchlaxpoliwag
-cp lab0/poliwag5 lab0/poochyena7/kabutops
-cat lab0/poochyena7/weezing lab0/poochyena7/galvantula > lab0/poliwag5_24
+chmod u+wx poliwag5
+chmod u+rx mienfoo6
+cp poliwag5 mienfoo6/munchlaxpoliwag
+ln -sf poliwag5 mienfoo6/munchlaxpoliwag
+chmod u-wx poliwag5
+cp poliwag5 poochyena7/kabutops
+cat poochyena7/weezing poochyena7/galvantula > poliwag5_24
 
-chmod 777 lab0/poochyena7
-ln lab0/rufflet7 lab0/poochyena7/weezingrufflet
-chmod 555 lab0/poochyena7 
+chmod u+w poochyena7
+ln rufflet7 poochyena7/weezingrufflet
+chmod u-w poochyena7 
 
-ln -s lab0/foongus4 lab0/Copy_51
+ln -s foongus4 Copy_51
 
-chmod u+r lab0/mienfoo6
-cp -r lab0/mienfoo6 lab0/mienfoo67
-cp -r lab0/mienfoo67 lab0/mienfoo6/hoothoot
-rm -rf lab0/mienfoo67
-# cp -r lab0/mienfoo6 lab0/mienfoo6/hoothoot невозможно, так директория будет копироваться сама в себя до бесконечности
-chmod u-r lab0/mienfoo6
+cp -r mienfoo6 mienfoo67
+cp -r mienfoo67 mienfoo6/hoothoot
+rm -rf mienfoo67
+# cp -r mienfoo6 mienfoo6/hoothoot невозможно, так директория будет копироваться сама в себя до бесконечности
 
-cd lab0
+chmod u+rx poochyena7/magneton
+echo -e "\nДерево после пункта 3:\n"
+ls -lR
+chmod u-rx mienfoo6
+chmod u-rx poochyena7/magneton
 #-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # Четвёртый пункт задания. Работа с файлами и сортировка.
 #-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 echo "1. Подсчет количества символов содержимого файлов: houndoom, munchlax, galvantula:"
-wc -m foongus4/houndoom mienfoo6/munchlax poochyena7/galvantula poochyena7/weezing 2>/dev/null>&1 | sort -r
+wc -m foongus4/houndoom mienfoo6/munchlax poochyena7/galvantula poochyena7/weezing 2>/dev/null | sort -r
 
 echo "2. Рекурсивный вывод список имен и атрибутов файлов в директории lab0, содержащих строку 'po':"
 ls -Rl | grep "po" | sort -n -k6 -r
 
-echo "3. Вывод содержимого файла wailord4 с номерами строk, заканчивающиеся на 'e':"
-cat -s wailord4 | grep -i '.*e$' | cat -n
+echo "3. Вывод содержимого файла wailord4 с номерами строк, заканчивающиеся на 'e':"
+cat wailord4 | grep -i 'e$' -n 2>&1
 
 echo "4. Вывод четырех последних элементов рекурсивного списка имен и атрибутов файлов в директории lab0, заканчивающихся на символ 'a':"
-ls -ltR | grep '.*a$' | head -n 4
+ls -lR | grep ".*a$" | head -n 4
 
 echo "5. Подсчёт количества символов содержимого файла rufflet7, запись результата в тот-же файл."
 wc -m rufflet7 >> rufflet7
@@ -124,13 +128,17 @@ wc -m rufflet7 >> rufflet7
 echo "6. Рекурсивный вывод файлов из lab0, имя которых заканчивается на '7':"
 ls -d *7 */*7 */*/*7 | sort 
 
-
 #--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # Пятый пункт задания. Удаление файлов и каталогов.
 #-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 rm rufflet7
-chmod -R 777 poochyena7 && rm -f poochyena7/weezing
-rm -f mienfoo6/munchlaxpoliw
-rm -f poochyena7/weezingruffl
-chmod -R 777 mienfoo6 && rm -rf mienfoo6
-rm -r poochyena7/kabutops
+chmod u+w poochyena7
+rm -f poochyena7/weezing
+rm -f mienfoo6/munchlaxpoliw*
+rm -f poochyena7/weezingruffl*
+chmod -R 777 mienfoo6 && rm -r mienfoo6
+chmod -R u+w poochyena7/kabutops && rm -r poochyena7/kabutops
+
+chmod u+r poochyena7/magneton
+echo -e "\nДерево после пункта 5:\n"
+ls -lR
